8fe98141c414602fc62e53bf7be933f63fcc9f31
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-27 16:36:28 +0300

    add views and change some urls

--- deck/urls.py
+++ deck/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from os import name
 from . import views
+from django.conf import settings
+
 
 urlpatterns = [
     url('',views.welcome,name='welcome'),
-    url('images/',views.images,name='images'),
+    url('images/',views.mapicha,name='images'),
     url('search/',views.search_results,name='search_results'),
-    url('picha/<int:image_id>/',views.image,name='imageid')
+    url('picha/<int:image_id>/',views.picha,name='imageid')
 ]
--- deck/views.py
+++ deck/views.py
@@ -1,7 +1,34 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from . models import Image
 
 
 # Create your views here.
 def welcome(request):
     return render(request, 'index.html')
+
+def mapicha(request):
+    images = Image.display_all_images()
+    return render(request,'all-images/mapicha.html',{"images":images},)
+
+def picha(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,'all-images/image.html',{"image":image})
+
+def search_results(request):
+    if 'tafutapicha' in request.GET and request.GET['tafutapicha']: #tafutapicha is what i will name my search element tag in the templates
+        category = request.GET.get('imagesearch')
+        images_searched = Image.search_by_category(category)
+        message = f"{category}"
+        print(images_searched)
+        return render(request, 'all-images/tafuta.html',{"message":message,"images":images_searched})
+    else:
+        message = "No search results available"
+        return render(request,'all-images/tafuta.html',{"message":message})
+
+
+
